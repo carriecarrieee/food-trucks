4d95fdf849cf96a57eba0ffdcd30c10274978911
Carrie Li <carrie.t.li@gmail.com>
2018-09-05 01:49:03 +0000

    Cleaned up code and added requirements file.

--- show_open_food_trucks.py
+++ show_open_food_trucks.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-
-# Make sure to install requests before running:
-# > pip install requests
-# Documentation for the requests library can be found here: http://docs.python-requests.org/en/master/
-
 import requests
 
 from pprint import pprint
@@ -14,14 +8,14 @@ from datetime import datetime
 def get_data():
     """Requests and accesses data from the San Francisco Data API, and 
        returns a python list of dictionaries."""
-    
+
     url = "http://data.sfgov.org/resource/bbb8-hzi6.json"
 
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json() # Converts json to python list of dictionaries
         return data
-    else:
+    else
         print "{} {}".format("Request failed; response code:", response.status_code)
 
 
